33ce88f0ee57f52530cfe1fec46ba81942595234
sheenazien <sheenazien08@gmail.com>
2020-06-22 23:51:16 +0700

    create AuthServiceProvider boot to check is it installed or not

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (!file_exists(base_path('.env'))) {
+            copy(base_path('.env.example'), base_path('.env'));
+        }
+        if (!env('APP_KEY')) {
+            Artisan::call('key:generate');
+        }
     }
 }
---
+++ routes/install.php
@@ -0,0 +1,5 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', 'Install\Install@show');
