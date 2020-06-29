09b865f8aa2422d34ade8d11450c34e989e88f50
sheenazien <sheenazien08@gmail.com>
2020-06-29 08:09:12 +0700

    bypass thorttle in CheckValidation

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/formvalidation', 'Api\CheckValidation');
+Route::post('/formvalidation', 'Api\CheckValidation')->withoutMiddleware('throttle');
