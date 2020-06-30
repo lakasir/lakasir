dbe1fcb4d6c60b44d8c12f2e1f0e107050977ad5
sheenazien <sheenazien08@gmail.com>
2020-07-01 06:56:09 +0700

    [BACKEND] fix install [FIXME] update env

--- app/Repositories/User.php
+++ app/Repositories/User.php
@@ -4,21 +4,42 @@ Namespace App\Repositories;
 
 use App\Abstracts\Repository as RepositoryAbstract;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Models\Role;
 
 class User extends RepositoryAbstract
 {
     protected string $model = 'App\Models\User';
 
+    /**
+     * @var string
+     */
+    private string $role = 'employee';
+
+
     public function create(Request $request)
     {
-        $session = $request->session()->all()['user'];
-        $request->merge($session);
-        $request->merge(['password' => bcrypt($request->password)]);
-        $user = new $this->model();
-        $user = $user->fill($request->all());
-        $user->save();
+        $self = $this;
+        return DB::transaction(static function() use ($request, $self) {
+            $session = $request->session()->all()['user'];
+            $request->merge($session);
+            $request->merge(['password' => bcrypt($request->password)]);
+            $user = new $self->model();
+            $user = $user->fill($request->all());
+            $user->save();
+            $role = Role::whereName($self->role)->first();
+            $user->assignRole($role);
 
-        return $user;
+            return $user;
+        });
     }
+
+    public function role(string $role): self
+    {
+        $this->role = $role;
+        return $this;
+    }
+
+
 }
 
