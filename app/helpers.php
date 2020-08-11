fae14fb4d9515f73af77d0b53adcc16be1be6d23
sheenazien <sheenazien08@gmail.com>
2020-08-11 12:13:57 +0700

    add helper dash_to_space

--- app/helpers.php
+++ app/helpers.php
@@ -1,6 +1,18 @@
 <?php
+
+use Illuminate\Support\Str;
+
 if (! function_exists('action')) {
-    function action(array $action) {
+    function action(array $action)
+    {
     }
 }
 
+if (! function_exists('dash_to_space')) {
+    function dash_to_space(string $string)
+    {
+        $name = str_replace('-', ' ', Str::title($string));
+        $name = str_replace('_', ' ', Str::title($name));
+        return $name;
+    }
+}
