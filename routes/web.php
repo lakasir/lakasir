18a7551638b25cf7e0432fef0b1838ff4d1814ec
sheenazien <sheenazien08@gmail.com>
2020-09-07 18:38:22 +0700

    pesan dari author

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
+/**
+ * Jika kamu tidak menyelesaikan ini, kamu punya hutang dengan diri kamu sendiri
+ * semangat, semua tujuan yang baik pasti akan menghasilkan output yang baik.
+ * proect ini gratis. boleh kamu jual dengan nama kamu, boleh kamu kustom sesuasi keinginan kamu, boleh kamu hapus tulisan ini,
+ * dan juga boleh kamu hapus licecnsinya.
+ *
+ * yang terakhir semoga kita diberikan kemudahan rizki dan hati
+ */
 Route::get('/', function () {
     return redirect()->to('/dashboard');
 })->middleware([ 'installed', 'auth' ]);
@@ -39,6 +35,9 @@ Route::group(['middleware' => [ 'installed', 'auth' ]], function () {
         Route::delete('/item/bulk-destroy', 'Master\Item@bulkDestroy');
         Route::resource('/item', 'Master\Item');
 
+
+        Route::get('/supplier/download-template', 'Master\Supplier@downloadTemplate')->name('supplier.download-template');
+        Route::post('/supplier/import', 'Master\Supplier@importTemplate')->name('supplier.import');
         Route::delete('/supplier/bulk-destroy', 'Master\Supplier@bulkDestroy');
         Route::resource('/supplier', 'Master\Supplier');
 
@@ -64,6 +63,7 @@ Route::group(['middleware' => [ 'installed', 'auth' ]], function () {
     Route::resource('/user', 'User\UserController');
 
     Route::group(['prefix' => 'transaction'], function () {
+        Route::get('/purchasing/{purchasing}/detail/{purchasing-detail}/edit', 'Transaction\Purchasing@editDetail')->name('purchasing.detail.edit');
         Route::resource('/purchasing', 'Transaction\Purchasing');
         Route::resource('/selling', 'Transaction\Selling');
         // Route::get('/cashier', 'Transaction\Selling@cashier');
