f703273e42ef7f69db5bfece5db3ed5e98992da6
Alfian Oktafireza <alfianokt104@gmail.com>
2021-07-22 01:01:10 +0700

    refactor: change repository to service

--- app/Http/Controllers/Master/Customer.php
+++ app/Http/Controllers/Master/Customer.php
@@ -8,7 +8,7 @@ use App\Http\Requests\Master\Customer\Browse;
 use App\Http\Requests\Master\Customer\Store;
 use App\Http\Requests\Master\Customer\Destroy;
 use App\Http\Requests\Master\Customer\Update;
-use App\Repositories\Customer as CustomerRepository;
+use App\Services\Customer as CustomerService;
 use App\Traits\Customer\CustomerTrait;
 use Illuminate\View\View;
 
@@ -23,7 +23,7 @@ class Customer extends Controller
      *
      * @return mix
      */
-    public function index(Browse $request, CustomerRepository $itemRepository)
+    public function index(Browse $request, CustomerService $itemRepository)
     {
         if ($request->ajax() || isset($this->return) && $this->return == 'api') {
             return $itemRepository->datatable($request);
@@ -48,11 +48,11 @@ class Customer extends Controller
 
 	/**
      * @param Store $request
-     * @param CustomerRepository $itemRepository
+     * @param CustomerService $itemRepository
      * @return RedirectResponse
      * @throws BindingResolutionException
      */
-    public function store(Store $request, CustomerRepository $itemRepository)
+    public function store(Store $request, CustomerService $itemRepository)
     {
         $itemRepository->create($request);
 
@@ -67,12 +67,12 @@ class Customer extends Controller
 
     /**
      * @param mixed $model
-     * @param CustomerRepository $itemRepository
+     * @param CustomerService $itemRepository
      * @param Browse $request
      * @return View|Factory
      * @throws BindingResolutionException
      */
-    public function show($model, CustomerRepository $itemRepository, Browse $request)
+    public function show($model, CustomerService $itemRepository, Browse $request)
     {
         $data = $itemRepository->find($model);
 
@@ -84,12 +84,12 @@ class Customer extends Controller
 
     /**
      * @param mixed $model
-     * @param CustomerRepository $itemRepository
+     * @param CustomerService $itemRepository
      * @param Update $request
      * @return View|Factory
      * @throws BindingResolutionException
      */
-    public function edit($model, CustomerRepository $itemRepository, Update $request)
+    public function edit($model, CustomerService $itemRepository, Update $request)
     {
         $data = $itemRepository->find($model);
 
@@ -101,13 +101,13 @@ class Customer extends Controller
 
     /**
      * @param string|int $model
-     * @param CustomerRepository $itemRepository
+     * @param CustomerService $itemRepository
      * @param Update $request
      * @return RedirectResponse
      * @throws AuthorizationException
      * @throws BindingResolutionException
      */
-    public function update($model, CustomerRepository $itemRepository, Update $request)
+    public function update($model, CustomerService $itemRepository, Update $request)
     {
         $data = $itemRepository->find($model);
 
@@ -124,12 +124,12 @@ class Customer extends Controller
 
     /**
      * @param mixed $model
-     * @param CustomerRepository $itemRepository
+     * @param CustomerService $itemRepository
      * @param Destroy $request
      * @return RedirectResponse
      * @throws BindingResolutionException
      */
-    public function destroy($model, CustomerRepository $itemRepository, Destroy $request)
+    public function destroy($model, CustomerService $itemRepository, Destroy $request)
     {
         $data = $itemRepository->find($model);
 
@@ -146,12 +146,12 @@ class Customer extends Controller
 
     /**
      * @param BulkDelete $request
-     * @param CustomerRepository $itemRepository
+     * @param CustomerService $itemRepository
      * @return Sheenazien8\Hascrudactions\Traits\Illuminate\Http\Response
      * @throws BindingResolutionException
      * @throws AuthorizationException
      */
-    public function bulkDestroy(BulkDelete $request, CustomerRepository $itemRepository)
+    public function bulkDestroy(BulkDelete $request, CustomerService $itemRepository)
     {
         $itemRepository->bulkDestroy($request);
 
