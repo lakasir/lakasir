63c93bca0cc9d5c8c85a189e3e2172a896d2784f
sheeenazien8 <sheenazien08@gmail.com>
2021-08-05 09:24:11 +0700

    add drop email in migration

--- database/migrations/2021_08_01_074306_add_column_email_to_suppliers_table.php
+++ database/migrations/2021_08_01_074306_add_column_email_to_suppliers_table.php
@@ -26,7 +26,7 @@ class AddColumnEmailToSuppliersTable extends Migration
     public function down()
     {
         Schema::table('suppliers', function (Blueprint $table) {
-            //
+            $table->dropColumn('email');
         });
     }
 }
