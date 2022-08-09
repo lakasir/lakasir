76ea48b85828f31f442365c83d02c78c2923073c
sheenazien8 <sheenazien08@gmail.com>
2022-08-09 07:51:43 +0700

    fixing master seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -3,8 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\Category;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CategorySeeder extends Seeder
 {
@@ -15,6 +15,7 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
         Category::truncate();
         Category::create([
             'name' => "UMUM"
--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Product;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ProductSeeder extends Seeder
 {
@@ -15,6 +16,7 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
         Product::truncate();
         Product::factory()->create();
     }
