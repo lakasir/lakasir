1ad926938271db389ba77307d577e38b6107a67a
sheenazien8 <8chelsealampard>
2022-08-17 18:29:22 +0700

    login page design (WIP)

--- next/pages/auth/login.tsx
+++ next/pages/auth/login.tsx
@@ -1,10 +1,9 @@
 import type { NextPage } from "next";
 import { FormEvent } from "react";
-import Button from "../../components/Buttons/Button";
-import Checkbox from "../../components/Fields/Checkbox";
-import Form from "../../components/Fields/Form";
-import Input from "../../components/Fields/Input";
-import Select from "../../components/Fields/Select";
+import PasswordField from "../../components/Auth/PasswordField";
+import { Button } from "../../components/Ui/Buttons/Button";
+import Form from "../../components/Ui/Fields/Form";
+import Input from "../../components/Ui/Fields/Input";
 
 const Login: NextPage = () => {
   const loginSubmit = (e: FormEvent, values: any) => {
@@ -16,35 +15,14 @@ const Login: NextPage = () => {
       <Form
         initialValue={{
           email: "sheenazien08@gmail.com",
-          date: "2022-08-07",
           password: "",
-          "remember-me": false,
-          countries: "SGP",
-          checkboxs: {
-            nameOne: true,
-            nameTwo: false,
-            nameThree: true,
-            nameFour: false,
-            nameFive: true,
-          }
         }}
         onSubmit={loginSubmit}
       >
         {() => (
           <>
             <Input name={"email"} type={"text"} label="Username or Email" />
-            <Input name={"date"} type={"date"} label="Date" />
-            <Input name={"password"} type={"password"} label="Password" />
-            <Select
-              name={"countries"}
-              label="Countries"
-              options={[
-                { label: "Indonesia", value: "IDN" },
-                { label: "Malaysia", value: "MLY" },
-                { label: "Singapore", value: "SGP" },
-              ]}
-            />
-            <Checkbox name={"remember-me"} label="Remember Me" />
+            <PasswordField/>
             <Button className="w-full py-4">Sign in</Button>
           </>
         )}
