7c5b606d4c124da62519f6a2366e4850794dd63f
sheenazien8 <sheenazien08@gmail.com>
2022-08-18 19:29:39 +0700

    fixing design menu page

--- next/pages/menu/index.tsx
+++ next/pages/menu/index.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from "next";
+import Image from "next/image";
 import Link from "next/link";
 import Layout from "../../components/Ui/Layout";
 
@@ -13,37 +14,37 @@ const menu: IMenuInterface[] = [
   {
     label: "Transaction",
     description: "Sell your product or service",
-    icon: "",
+    icon: <img src={"./assets/icons/Cash.svg"} width="50" height="50"/>,
     href: "/menu/transaction",
   },
   {
     label: "Product",
     description: "Show list your product.",
-    icon: "",
+    icon: <img src="./assets/icons/Shipping Product.svg" width="50" height="50"/>,
     href: "/menu/product",
   },
   {
     label: "Member",
     description: "Member is a success key for your  bussiness",
-    icon: "",
+    icon: <img src="./assets/icons/Leader.svg" width="50" height="50"/>,
     href: "/menu/member",
   },
   {
     label: "Profile",
     description: "Every person is has an iconic profile",
-    icon: "",
+    icon: <img src="./assets/icons/User.svg" width="50" height="50"/>,
     href: "/menu/profile",
   },
   {
     label: "About",
     description: "Every bussiness is has an iconic purpose",
-    icon: "",
+    icon: <img src="./assets/icons/Info.svg" width="50" height="50"/>,
     href: "/menu/about",
   },
   {
     label: "Setting",
     description: "Lakasir is configurable",
-    icon: "",
+    icon: <img src="./assets/icons/Settings.svg" width="50" height="50"/>,
     href: "/menu/setting",
   },
 ];
@@ -52,17 +53,17 @@ const Menu: NextPage = () => {
   return (
     <Layout title="Menu">
       <div className="py-9 space-y-8">
-        {menu.map((m) => (
-          <Link href={m.href}>
+        {menu.map((m, index) => (
+          <Link href={m.href} key={index}>
             <a className="block">
               <div className="w-full h-24 rounded-lg overflow-hidden cursor-pointer">
-                <div className="bg-[#E0DCFD] rounded-lg w-1/4 h-24 absolute flex items-center justify-center">
-                  {/* <CashIcon className="text-gray-500 h-16 w-16" /> */}
+                <div className="bg-lakasir-primary rounded-lg w-[91px] h-[93px] absolute flex items-center justify-center">
+                  {m.icon}
                 </div>
                 <div className="flex">
-                  <div className="bg-[#E0DCFD] rounded-lg w-1/4 h-24"></div>
-                  <div className="bg-[#F1EFFF] w-3/4 flex">
-                    <div className="items-center ml-5 mt-5">
+                  <div className="bg-lakasir-primary rounded-lg w-[91px] h-[93px]"></div>
+                  <div className="w-3/4 flex">
+                    <div className="items-center ml-7 mt-5">
                       <p className="text-xl">{m.label}</p>
                       <p className="font-light text-sm">{m.description}</p>
                     </div>
