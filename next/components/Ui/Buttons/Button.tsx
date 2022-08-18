183a07ed6394d5ee0b49c314bfdac614be721a4f
sheenazien8 <sheenazien08@gmail.com>
2022-08-18 19:29:05 +0700

    fixing ui layout and button

--- next/components/Ui/Buttons/Button.tsx
+++ next/components/Ui/Buttons/Button.tsx
@@ -3,14 +3,16 @@ import { classNames } from "../../../utils/helpers";
 export interface ButtonProps {
   children: string | string[] | JSX.Element[] | JSX.Element;
   className?: string;
+  onClick?: () => void;
 }
 
 function Button(props: ButtonProps): JSX.Element {
   return (
     <button
+      onClick={props.onClick}
       className={classNames(
         props.className,
-        "my-3 items-center border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-white bg-lakasir-primary outline-none focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-lakasir-primary"
+        "my-3 items-center border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-white bg-lakasir-primary outline-none"
       )}
     >
       {props.children}
--- next/components/Ui/Layout.tsx
+++ next/components/Ui/Layout.tsx
@@ -1,10 +1,14 @@
+import { ArrowLeftIcon } from "@heroicons/react/solid";
 import Head from "next/head";
+import { useRouter } from "next/router";
 interface ILayoutInterface {
   children: JSX.Element;
   title?: string;
+  back?: boolean;
 }
 
 const Layout = (props: ILayoutInterface) => {
+  const router = useRouter();
   return (
     <>
       <Head>
@@ -19,9 +23,16 @@ const Layout = (props: ILayoutInterface) => {
           rel="stylesheet"
         />
       </Head>
-      {props.title ? (
-        <div className="flex justify-center items-center max-h-14 h-[52px] font-semibold text-xl">
-          {props.title}
+      {props.title || props.back ? (
+        <div className="flex items-center max-h-14 h-[52px] font-semibold text-xl mx-auto w-11/12">
+          {props.back ? (
+            <span className="cursor-pointer" onClick={() => router.back()}>
+              <ArrowLeftIcon className="h-5 w-5" />
+            </span>
+          ) : (
+            ""
+          )}
+          <p className="text-center w-full mr-4">{props.title}</p>
         </div>
       ) : (
         ""
