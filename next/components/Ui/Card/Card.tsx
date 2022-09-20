1d297d16e8e3ce345da1d52d7bec44bc4e635272
sheenazien8 <8chelsealampard>
2022-09-20 14:56:19 +0700

    refactor the card ui

--- next/components/Ui/Card/Card.tsx
+++ next/components/Ui/Card/Card.tsx
@@ -1,11 +1,8 @@
-import { MouseEventHandler, useState } from "react";
-import { classNames } from "../../../utils/helpers";
+import { MouseEventHandler } from "react";
 
-interface ClassName {
-  confirmable?: {
-    confirm?: string;
-    cancel?: string;
-  };
+interface IActionOption {
+  label: string;
+  onClick: () => void;
 }
 
 interface ICardInterface {
@@ -15,117 +12,44 @@ interface ICardInterface {
   label: string;
   description?: string;
   sub_description?: string;
-  action?: JSX.Element;
+  action?: IActionOption[];
   id?: number;
-  class?: ClassName;
-  disable?: ActionAble;
-}
-
-interface ActionAble {
-  confirmable: boolean;
 }
 
 const Card = (props: ICardInterface) => {
-  const [disable, setDisable] = useState<ActionAble>({
-    confirmable: false,
-  });
-
   return (
-    <div className="block relative" id="action-detail">
-      {props.confirmable && !disable.confirmable ? (
-        <div
-          className={classNames(
-            "w-full rounded-lg overflow-hidden cursor-pointer hidden"
+    <div className="flex" id="action-detail">
+      <div
+        className="w-full rounded-lg overflow-hidden cursor-pointer"
+        id={`item-list-${props.id}`}
+      >
+        <div className="flex items-center" onClick={props.onClick}>
+          {props.image ? (
+            <div className="bg-lakasir-primary w-1/5 rounded-lg mr-2 flex justify-center items-center">
+              {props.image}
+            </div>
+          ) : (
+            ""
           )}
-          id={`action-confirm-${props.id}`}
-        >
-          <div
-            className={classNames(
-              props.class?.confirmable?.confirm,
-              "w-1/2 flex items-center justify-center my-auto text-2xl font-semibold bg-red-600 h-full text-white"
-            )}
-            onClick={() => {
-              document
-                .querySelector(`#action-confirm-${props.id}`)
-                ?.classList.add("hidden");
-              document
-                .querySelector(`#item-list-${props.id}`)
-                ?.classList.remove("hidden");
-
-              if (props.confirmable != undefined) {
-                props.confirmable();
-              }
-            }}
-          >
-            Confirm!
+          <div className="items-center w-4/5">
+            <p className="text-lg">{props.label}</p>
+            <p className="font-extralight text-sm">{props.description}</p>
+            <p className="font-light text-sm">{props.sub_description}</p>
           </div>
-          <div
-            className={classNames(
-              props.class?.confirmable?.cancel,
-              "w-1/2 flex items-center justify-center my-auto text-2xl font-semibold h-full bg-gray-200"
-            )}
-            id={`cancel-confirm`}
-            onClick={() => {
-              document
-                .querySelector(`#action-confirm-${props.id}`)
-                ?.classList.remove("flex");
-              document
-                .querySelector(`#action-confirm-${props.id}`)
-                ?.classList.add("hidden");
-              document
-                .querySelector(`#item-list-${props.id}`)
-                ?.classList.remove("hidden");
-            }}
+        </div>
+      </div>
+      {props.action ? (
+        <div className="flex flex-col justify-center items-center">
+          <a
+            href="#"
+            className="w-full rounded-full border border-gray-300 bg-white px-2.5 py-0.5 text-sm font-medium leading-5 text-gray-700 shadow-sm hover:bg-gray-50"
           >
-            Cancel!
-          </div>
+            Action
+          </a>
         </div>
       ) : (
         ""
       )}
-      <div
-        onClick={props.onClick}
-        className="w-full rounded-lg overflow-hidden cursor-pointer"
-        id={`item-list-${props.id}`}
-      >
-        {props.image ? (
-          <div className="bg-lakasir-primary rounded-lg w-[91px] h-[93px] absolute flex items-center justify-center overflow-hidden mr-7">
-            {props.image}
-          </div>
-        ) : (
-          ""
-        )}
-        <div className="flex items-center">
-          {props.image ? <div className="w-1/2 h-[93px] mr-7"></div> : ""}
-          <div className="items-center w-full">
-            <p className="text-xl">{props.label}</p>
-            <p className="font-extralight text-sm">{props.description}</p>
-            <p className="font-light text-sm">{props.sub_description}</p>
-          </div>
-          <div
-            className="w-1/2 h-full"
-            onClick={() => {
-              document
-                .querySelector(`#action-confirm-${props.id}`)
-                ?.classList.remove("hidden");
-              document
-                .querySelector(`#action-confirm-${props.id}`)
-                ?.classList.add("flex");
-              if (!props.disable?.confirmable) {
-                document
-                  .querySelector(`#item-list-${props.id}`)
-                  ?.classList.add("hidden");
-              }
-
-              if (props.disable?.confirmable) {
-                setDisable({ confirmable: true });
-              }
-            }}
-          >
-            {props.action ? props.action : ""}
-          </div>
-        </div>
-      </div>
     </div>
   );
 };
