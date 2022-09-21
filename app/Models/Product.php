c13f08315affcb67a1335e224ceb4f6dd8bcfd22
sheenazien8 <8chelsealampard>
2022-09-21 18:23:26 +0700

    add category relation

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,4 +10,9 @@ class Product extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
