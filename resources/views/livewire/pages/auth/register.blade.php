af60225c25bedcbd7661afaacb67c857a5e8d4d3
sheenazien8 <sheenazien08@gmail.com>
2024-04-02 22:08:26 +0700

    add landing page

--- resources/views/livewire/pages/auth/register.blade.php
+++ resources/views/livewire/pages/auth/register.blade.php
@@ -1,92 +1,102 @@
 <?php
 
-use App\Models\User;
-use App\Providers\RouteServiceProvider;
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rules;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Wizard;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Forms\Form;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\HtmlString;
+use Livewire\Attributes\Layout;
+use Livewire\Volt\Component;
 
-use function Livewire\Volt\layout;
-use function Livewire\Volt\rules;
-use function Livewire\Volt\state;
+new
+#[Layout('livewire.components.layouts.guest')]
+class extends Component implements HasForms
+{
+    use InteractsWithForms;
 
-layout('layouts.guest');
+    public ?array $data = [];
 
-state([
-    'name' => '',
-    'email' => '',
-    'password' => '',
-    'password_confirmation' => ''
-]);
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Wizard::make([
+                    Wizard\Step::make('Akun pemilik')
+                        ->schema([
+                            TextInput::make('full_name')
+                                ->label('Nama Lengkap')
+                                ->string()
+                                ->required(),
+                            TextInput::make('email')
+                                ->label('Email')
+                                ->email()
+                                ->required(),
+                            TextInput::make('password')
+                                ->password()
+                                ->required()
+                                ->rules(['confirmed']),
+                            TextInput::make('password_confirmation')
+                                ->password(),
+                        ])
+                        ->columns(1)
+                        ->icon('heroicon-o-user'),
+                    Wizard\Step::make('Detail Toko')
+                        ->schema([
+                            TextInput::make('store_name')
+                                ->label('Nama Toko')
+                                ->string()
+                                ->required(),
+                            TextInput::make('store_address')
+                                ->label('Alamat Toko')
+                                ->string()
+                                ->required(),
+                            TextInput::make('store_phone')
+                                ->label('Nomor Telepon')
+                                ->string()
+                                ->required(),
+                        ])
+                        ->icon('heroicon-o-shopping-bag'),
+                    Wizard\Step::make('Domain Toko')
+                        ->schema([
+                            TextInput::make('domain')
+                                ->label('Domain')
+                                ->rules(['unique:tenants,id'])
+                                ->live(debounce: 500)
+                                ->suffix('.'.config('tenancy.central_domains')[0]),
+                        ])
+                        ->icon('heroicon-o-globe-alt'),
+                ])
+                    ->submitAction(new HtmlString(
+                        Blade::render(<<<'BLADE'
+                                    <x-filament::button
+                                        type="submit"
+                                        size="sm"
+                                        wire:click="create"
+                                    >
+                                        Submit
+                                    </x-filament::button>
+                                  BLADE))),
+            ])
+            ->statePath('data');
+    }
 
-rules([
-    'name' => ['required', 'string', 'max:255'],
-    'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
-    'password' => ['required', 'string', 'confirmed', Rules\Password::defaults()],
-]);
-
-$register = function () {
-    $validated = $this->validate();
-
-    $validated['password'] = Hash::make($validated['password']);
-
-    event(new Registered($user = User::create($validated)));
-
-    Auth::login($user);
-
-    $this->redirect(RouteServiceProvider::HOME, navigate: true);
-};
+    public function create(): void
+    {
+        dd($this->form->getState());
+    }
+}
 
 ?>
 
-<div>
-    <form wire:submit="register">
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input wire:model="name" id="name" class="block mt-1 w-full" type="text" name="name" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div>
-
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input wire:model="email" id="email" class="block mt-1 w-full" type="email" name="email" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input wire:model="password" id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input wire:model="password_confirmation" id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}" wire:navigate>
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
+<div class="max-w-xl mx-auto flex justify-center items-center h-screen">
+  <div>
+    <div class="flex justify-center items-center my-10">
+      <img src="{{ asset('assets/logo/image.png') }}" class="w-20 h-24" alt="Logo">
+    </div>
+    <div class="w-full">
+      {{ $this->form }}
+    </div>
+  </div>
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 import defaultTheme from "tailwindcss/defaultTheme";
+import preset from './vendor/filament/support/tailwind.config.preset'
+
 import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
+  presets: [preset],
   content: [
     "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
     "./storage/framework/views/*.php",
     "./resources/views/**/*.blade.php",
+    './app/Filament/**/*.php',
   ],
 
   theme: {
