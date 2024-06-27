719afb6e8936de064dfabb44068cae27278cc959
sheenazien8 <sheenazien08@gmail.com>
2024-06-27 20:04:31 +0700

    feature(admin): remove cms builder

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -55,7 +55,6 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ])
-            ->plugin(\TomatoPHP\FilamentCms\FilamentCMSPlugin::make());
+            ]);
     }
 }
