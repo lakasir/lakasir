838a1d3680a47968f30c1ca13a4db20d0bb9cad6
sheenazien8 <sheenazien08@gmail.com>
2024-07-05 15:03:35 +0700

    fix message for about image

--- resources/views/forms/components/image-preview.blade.php
+++ resources/views/forms/components/image-preview.blade.php
@@ -3,6 +3,11 @@
   :field="$field">
   <div x-data="{ state: $wire.$entangle('{{ $getStatePath() }}') }">
     <!-- Interact with the `state` property in Alpine.js -->
-    <img :src="state" alt="images" width="200" class="border border-gray-300 dark:border-gray-700 rounded-lg">
+    <template x-if="state">
+      <img :src="state" alt="images" width="200" class="border border-gray-300 dark:border-gray-700 rounded-lg">
+    </template>
+    <template x-if="!state">
+      <p class="text-3xl">@lang('Empty')</p>
+    </template>
   </div>
 </x-dynamic-component>
