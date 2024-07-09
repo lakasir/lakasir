b25e16e11d26bbafa7bf1786cbf970760bb7c87f
sheenazien8 <sheenazien08@gmail.com>
2024-07-10 05:53:16 +0700

    fix(register): form should be nice

--- resources/views/livewire/forms/auth/register.blade.php
+++ resources/views/livewire/forms/auth/register.blade.php
@@ -1,10 +1,15 @@
-<div class="max-w-xl mx-auto flex justify-center items-center h-screen">
-  <div>
-    <div class="flex justify-center items-center my-10">
-      <img src="{{ asset('assets/logo/image.png') }}" class="w-20 h-24" alt="Logo">
-    </div>
-    <div class="w-full">
-      {{ $this->form }}
+<div>
+  <div class="max-w-2xl mx-auto grid grid-cols-1 justify-center items-center h-screen">
+    <div class="space-y-6">
+      <div class="flex justify-center items-center">
+        <img src="{{ asset('assets/logo/image.png') }}" class="w-20 h-24" alt="Logo">
+      </div>
+      <div class="w-full">
+        <form>
+          {{ $this->form }}
+        </form>
+        <x-filament-actions::modals />
+      </div>
     </div>
   </div>
 </div>
