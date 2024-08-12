ec53bc66ce3e3bad320c8fcc833802e719266875
sheenazien8 <sheenazien08@gmail.com>
2024-08-12 19:01:26 +0700

    fix jobs table in testing

--- database/migrations/2024_08_10_012803_create_jobs_table.php
+++ database/migrations/2024_08_10_012803_create_jobs_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::dropIfExists('jobs');
         Schema::create('jobs', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('queue')->index();
