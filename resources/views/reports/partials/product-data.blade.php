be76a9bdf033983c63b88e22040de852590fb5b1
sheenazien8 <sheenazien08@gmail.com>
2024-08-27 11:38:17 +0700

    product report in html view

--- resources/views/reports/partials/product-data.blade.php
+++ resources/views/reports/partials/product-data.blade.php
@@ -1,32 +1,33 @@
 <div class="max-w-full">
   <div class="text-center space-y-2">
-    <h1 class="text-3xl font-semibold">{{ __('Selling Report') }}</h1>
+    <h1 class="text-3xl font-semibold">{{ __('Product Report') }}</h1>
     <h3 class="text-xl">{{ $header['shop_name'] }}</h3>
   </div>
   <p class="mb-4">{{ __('Period') }}: <b>{{ $header['start_date'] }} - {{ $header['end_date'] }}</b></p>
-
   <x-table class="w-full table-fixed">
     <x-table-header>
-      <x-table-header-cell>@lang('SKU')</x-table-header-cell>
-      <x-table-header-cell>@lang('Product Name')</x-table-header-cell>
-      <x-table-header-cell>@lang('Price')</x-table-header-cell>
-      <x-table-header-cell>@lang('Qty')</x-table-header-cell>
-      <x-table-header-cell>@lang('Selling')</x-table-header-cell>
-      <x-table-header-cell>@lang('Discount')</x-table-header-cell>
-      <x-table-header-cell>@lang('Net Selling')</x-table-header-cell>
-      <x-table-header-cell>@lang('Gross Profit')</x-table-header-cell>
-      <x-table-header-cell>@lang('Net Profit')</x-table-header-cell>
+      <x-table-header-cell>SKU</x-table-header-cell>
+      <x-table-header-cell>{{ __('Product Name') }}</x-table-header-cell>
+      <x-table-header-cell style="width: 100px;" class="number">{{ __('Price') }}</x-table-header-cell>
+      <x-table-header-cell>{{ __('Qty') }}</x-table-header-cell>
+      <x-table-header-cell style="width: 100px;" class="number">{{ __('Selling') }}</x-table-header-cell>
+      <x-table-header-cell style="width: 100px;" class="number">{{ __('Discount') }}</x-table-header-cell>
+      <x-table-header-cell style="width: 100px;" class="number">{{ __('Net Selling') }}</x-table-header-cell>
+      <x-table-header-cell style="width: 100px;" class="number">{{ __('Gross Profit') }}</x-table-header-cell>
+      <x-table-header-cell style="width: 100px;" class="number">{{ __('Net Profit') }}</x-table-header-cell>
     </x-table-header>
-
     <tbody>
       @foreach($reports as $key => $report)
         <x-table-row>
+          <!-- <td>{{ $report['code'] }}</td> -->
           <x-table-cell>{{ $report['sku'] }}</x-table-cell>
           <x-table-cell>{{ $report['name'] }}</x-table-cell>
+          <!-- <td class="number">{{ $report['initial_price'] }}</td> -->
           <x-table-cell class="number">{{ $report['selling_price'] }}</x-table-cell>
           <x-table-cell>{{ $report['qty'] }}</x-table-cell>
           <x-table-cell class="number">{{ $report['selling'] }}</x-table-cell>
           <x-table-cell class="number">{{ $report['discount_price'] }}</x-table-cell>
+          <!-- <td class="number">{{ $report['cost'] }}</td> -->
           <x-table-cell class="number">{{ $report['total_after_discount'] }}</x-table-cell>
           <x-table-cell class="number">{{ $report['gross_profit'] }}</x-table-cell>
           <x-table-cell class="number">{{ $report['net_profit'] }}</x-table-cell>
@@ -47,7 +48,7 @@
   <x-table class="w-full table-fixed mt-4">
     <x-table-header>
       <x-table-row>
-        <x-table-header-cell colspan="8" class="text-center" style="text-align: center;">{{ __('Grand Total') }}</x-table-header-cell>
+        <x-table-header-cell colspan="8" style="text-align: center;">{{ __('Grand Total') }}</x-table-header-cell>
       </x-table-row>
       <x-table-row>
         <x-table-header-cell>{{ __('Cost') }}</x-table-header-cell>
@@ -73,5 +74,5 @@
       </x-table-row>
     </tbody>
   </x-table>
-</div>
 
+</div>
