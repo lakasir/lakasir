ff450348aaa092b8cf9fb897da6de220a839d20b
sheenazien8 <sheenazien08@gmail.com>
2025-01-08 07:45:38 +0700

    show demo account in local environment

--- app/Filament/Tenant/Pages/TenantLogin.php
+++ app/Filament/Tenant/Pages/TenantLogin.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Tenant\Pages;
 
 use Filament\Facades\Filament;
+use Filament\Forms\Components\Component;
 use Filament\Http\Responses\Auth\LoginResponse;
 use Filament\Pages\Auth\Login;
 use Illuminate\Validation\ValidationException;
@@ -14,7 +15,7 @@ class TenantLogin extends Login
         $loginResponse = parent::authenticate();
         /** @var \App\Models\Tenants\User $user */
         $user = Filament::auth()->user();
-        if (! $user->can('access web app')) {
+        if (!$user->can('access web app')) {
             throw ValidationException::withMessages([
                 'data.email' => 'You do not have permission to access the web app',
             ]);
@@ -31,5 +32,19 @@ class TenantLogin extends Login
         );
 
         return $loginResponse;
+    }
+
+    public function mount(): void
+    {
+        if (Filament::auth()->check()) {
+            redirect()->intended(Filament::getUrl());
+        }
+
+        if (app()->environment('demo')) {
+            $this->form->fill([
+                'email' => 'demo@lakasir.com',
+                'password' => 'passwordsangatrahasia'
+            ]);
+        }
     }
 }
