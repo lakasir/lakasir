2c25ded665399409d959d249184e2fad8b330666
sheenazien8 <sheenazien08@gmail.com>
2025-10-10 23:51:23 +0700

    init feature

---
+++ app/Features/Qris.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Features;
+
+class Qris
+{
+    /**
+     * Resolve the feature's initial value.
+     */
+    public function resolve(mixed $_scope): mixed
+    {
+        return true;
+    }
+}
